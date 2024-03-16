72bc13db8c3207960798bf4de480a9bf1087f1a4
azenizzka <azenizzka@gmail.com>
2024-03-17 01:47:32 +0700

    init

---
+++ hyprland-dracula/install.sh
@@ -0,0 +1,75 @@
+# !/bin/bash
+
+function main() {
+    # enableMultilib
+
+    # installUtils
+
+    # installYay
+
+    # makeAudio
+
+    installDrivers
+
+    installWM
+
+    installTerminal
+
+    # installFonts
+
+    installConfigs
+}
+
+function enableMultilib() {
+    sudo cp etc/pacman.conf /etc
+    sudo pacman -Suy
+}
+
+function installUtils() {
+    sudo pacman -S git
+}
+
+function installYay() {
+    mkdir build
+    cd build
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+    cd ../..
+    rm -rf build
+}
+
+function makeAudio() {
+    sudo pacman -S pipewire pipewire-pulse wireplumber
+}
+
+function installFonts() {
+    sudo pacman -S ttf-jetbrains-mono-nerd noto-fonts-emoji
+}
+
+function installDrivers() {
+    sudo pacman -S mesa vulkan-intel
+}
+
+function installTerminal() {
+    sudo pacman -S alacritty fish
+}
+
+function installWM() {
+    sudo pacman -S hyprland hyprpaper waybar xdg-desktop-portal-hyprland grim slurp xorg-xwayland
+
+    yay -S hyprpicker
+}
+
+function installConfigs() {
+    chsh -s /usr/bin/fish
+
+    cp -r .fonts/ ~/
+    cp -r .config/ ~/
+
+    fc-cache -fv
+}
+
+
+
+main
