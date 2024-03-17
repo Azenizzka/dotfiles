35babe16d64dcc12c0350fbf107f9a149b69873d
azenizzka <azenizzka@gmail.com>
2024-03-17 17:39:18 +0700

    a

--- hyprland-dracula/install.sh
+++ hyprland-dracula/install.sh
@@ -65,7 +65,6 @@ function installApps() {
 }
 
 function installFishPM() {
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 }
 
