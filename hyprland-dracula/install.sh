60b3ac3dbe74fe19637b8922b6e4aa108cf59196
azenizzka <azenizzka@gmail.com>
2024-03-27 17:02:18 +0700

    delete discord (use firefox)

--- hyprland-dracula/install.sh
+++ hyprland-dracula/install.sh
@@ -58,7 +58,7 @@ function installWM() {
 }
 
 function installApps() {
-    sudo pacman -S openssh firefox jre17-openjdk discord telegram-desktop btop neofetch filezilla unzip zip libreoffice-fresh docker docker-compose man nano
+    sudo pacman -S openssh firefox jre17-openjdk telegram-desktop btop neofetch filezilla unzip zip libreoffice-fresh docker docker-compose man nano
 
     yay -S visual-studio-code-bin intellij-idea-ultimate-edition postman-bin
 }
