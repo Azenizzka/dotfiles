34f635f537f5f4c63115349442b26b2379c9b3d2
azenizzka <azenizzka@gmail.com>
2024-03-28 19:01:03 +0700

    add screenlocker

--- hyprland-dracula/install.sh
+++ hyprland-dracula/install.sh
@@ -52,7 +52,7 @@ function installTerminal() {
 }
 
 function installWM() {
-    sudo pacman -S hyprland hyprpaper waybar xdg-desktop-portal-hyprland xdg-desktop-portal-wlr xdg-desktop-portal xorg-xwayland mako grim slurp wl-clipboard wofi
+    sudo pacman -S hyprland hyprpaper waybar xdg-desktop-portal-hyprland xdg-desktop-portal-wlr xdg-desktop-portal xorg-xwayland mako grim slurp wl-clipboard wofi brightnessctl hypridle hyprlock
 
     yay -S hyprpicker
 }
