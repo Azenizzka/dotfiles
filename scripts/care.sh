e08fd3ef3e6f0842543ee8c628d8039e306739b1
azenizzka <azenizzka.ru>
2024-05-19 21:04:36 +0700

    ngr

--- scripts/care.sh
+++ scripts/care.sh
@@ -7,7 +7,14 @@ yay -Suy
 
 sudo pacman -Scc
 
+sudo paccache -r
+sudo paccache -rk1
+sudo paccache -ruk0
+
 sudo pacman -Rsn $(pacman -Qdtq)
+
+sudo truncate -s 0 /var/log/pacman.log
 
 rm -rf ~/.cache/*
 rm -rf ~/.local/share/Trash
+sudo rm -rf /tmp/*
