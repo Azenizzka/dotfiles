9183f38c606dd076d51426b11846823ee443f1a0
Evgeny <119114805+Azenizzka@users.noreply.github.com>
2024-09-07 22:20:29 +0700

    Update config.fish

--- hyprland-dracula/.config/fish/config.fish
+++ hyprland-dracula/.config/fish/config.fish
@@ -1,6 +1,10 @@
+alias ls "eza"
+alias ll "eza -la --group-directories-first -rs=size --total-size --no-user --icons=always --colour=always --colour-scale-mode=fixed"
+
 export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
+
 if status is-login
-if test -z "$DISPLAY" -a "$(tty)" = /dev/tty1
-dbus-run-session Hyprland
-end
+    if test -z "$DISPLAY" -a "$(tty)" = /dev/tty1
+        dbus-run-session Hyprland
+    end
 end
